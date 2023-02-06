212491cdad9de7170d7abdce4051040943dc31ae
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-02-07 01:06:07 +0900

    commit

--- BaekJoon#10816/main.swift
+++ BaekJoon#10816/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution() {
+    
+    let N = Int(readLine()!)!
+    var nArr = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let M = Int(readLine()!)!
+    let mArr = readLine()!.split(separator: " ").map { Int(String($0))! }
+    
+    nArr.sort()
+    
+    var dict: [Int: Int] = [:]
+    var result: [Int] = []
+    
+    for i in nArr {
+        if dict.keys.contains(i) { dict[i]! += 1 }
+        else { dict[i] = 1 }
+    }
+    
+    for i in mArr {
+        if dict.keys.contains(i) { result.append(dict[i]!) }
+        else { result.append(0) }
+    }
+    
+    print(result.map { String($0) }.joined(separator: " "))
+}
 
+solution()
